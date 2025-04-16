f3d78f50ea3b1a0b7880c90ee376dd998d96a12a
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-16 22:23:35 +0300

    was able to create my list

--- 01tutorial/src/createList.js
+++ 01tutorial/src/createList.js
@@ -21,7 +21,20 @@ function CreateList() {
     },
   ]);
 
-  return <main></main>;
+  // Please Note that keys are very important in creating list items for react as it is a way which react identify items that has changed.
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked} />
+            <label>{item.item}</label>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default CreateList;
