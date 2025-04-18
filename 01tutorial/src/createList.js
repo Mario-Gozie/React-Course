9d2b9bc160efdb3cddbaada59050d9c687185826
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-18 11:48:21 +0300

    was able to understand state

--- 01tutorial/src/createList.js
+++ 01tutorial/src/createList.js
@@ -7,7 +7,7 @@ function CreateList() {
   const [items, setItems] = useState([
     {
       id: 1,
-      checked: false,
+      checked: true,
       item: "One half pound bag of Cocoa Covered",
     },
     {
@@ -23,18 +23,55 @@ function CreateList() {
   ]);
 
   // Please Note that keys are very important in creating list items for react as it is a way which react identify items that has changed.
+  const handleCheck = (id) => {
+    // Here I am saying when I click, go to all the when there is a click, look at the array of items, if any item id matches Id passed to the handle click, create a new item and replace checked with its oposite value which could be true or false. else, just return the item.
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+
+    // Now we can use set item to change the state of the items
+
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify("listItems"));
+  };
+
+  const handleDelete = (id) => {
+    // This filters out and returns ids that dont match Id passed into it. this is used in a case of delete
+    const listItems = items.filter((item) => item.id !== id);
+
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify("listItems"));
+  };
+
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input type="checkbox" checked={item.checked} />
-            <label>{item.item}</label>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
 
-            <FaTrash role="button" tabIndex="0" />
-          </li>
-        ))}
-      </ul>
+              <FaTrash
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty</p>
+      )}
     </main>
   );
 }
