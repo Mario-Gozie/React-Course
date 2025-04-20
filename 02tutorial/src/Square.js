e53dd18c3633ef0ed1fa879f3e5fd1afe7deabaa
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-20 23:13:25 +0300

    I have been able to work on a little project to practice props very well

---
+++ 02tutorial/src/Square.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Square = ({ colorValue, hexValue, isDarkText }) => {
+  return (
+    <section
+      className="square"
+      style={{ background: colorValue, color: isDarkText ? "#000" : "#FFF" }}
+    >
+      <p>{colorValue ? colorValue : "Empty Value"}</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
+  );
+};
+
+Square.defaultProps = {
+  colourValue: "Empty Color Value",
+};
+export default Square;
