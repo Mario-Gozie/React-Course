28468c0c69d9fe12a0002baf0cc88a16cfed71e6
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-26 23:23:48 +0300

    working on an app and use effect

--- 03tutorial/src/App.js
+++ 03tutorial/src/App.js
@@ -5,8 +5,25 @@ function App() {
   const [reqType, setReqType] = useState("users");
   const [items, setItems] = useState([]);
 
-  useEffect(() => {}, []);
-  return <div className="App"></div>;
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchItems();
+  }, [reqType]);
+
+  return (
+    <div className="App">
+      <Form reqType={request}></Form>
+    </div>
+  );
 }
 
 export default App;
