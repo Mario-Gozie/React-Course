c686767b34c8d8eef0e556c33ca16472247cb782
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-27 00:19:12 +0300

    was able to add css

--- 03tutorial/src/App.js
+++ 03tutorial/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Form } from "./Form";
+import { List } from "./List";
 
 function App() {
   const API_URL = "https://jsonplaceholder.typicode.com/";
