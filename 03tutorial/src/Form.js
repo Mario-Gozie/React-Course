73f5565b6cb3bca403b39cd2f0e22848a840b10c
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-26 23:34:56 +0300

    working on the button

--- 03tutorial/src/Form.js
+++ 03tutorial/src/Form.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { Button } from "./Button";
 
 export const Form = ({ reqType, setReqType }) => {
-  return <form onSubmit={(e) => e.preventDefault()}></form>;
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <Button />
+      <Button />
+      <Button />
+    </form>
+  );
 };
