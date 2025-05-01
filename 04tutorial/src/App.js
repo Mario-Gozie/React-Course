8bcb729eccd0e51eed946d5c23c4ec27bf08bc36
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-01 11:41:44 +0300

    I was able to fix some bug with rendering data

--- 04tutorial/src/App.js
+++ 04tutorial/src/App.js
@@ -1,15 +1,28 @@
+import React from "react";
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import Home from "./NewPost";
+import Home from "./Home";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Switch, useHistory } from "react-router-dom";
-import { useState, useEffect } from "react";
+import NewPost from "./NewPost";
+// import { Route, Switch, useHistory } from "react-router-dom";
+// import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
