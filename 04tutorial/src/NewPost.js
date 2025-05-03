40e0906afed9bff126af3d3e84e9aac5eae874d4
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-03 22:54:15 +0300

    setting up the new post page

--- 04tutorial/src/NewPost.js
+++ 04tutorial/src/NewPost.js
@@ -10,7 +10,24 @@ function NewPost({
   return (
     <main className="NewPost">
       <h2>New Post</h2>
-      <form className="newPostForm" onSubmit={handleSubmit}></form>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          id="postTitle"
+          type="text"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        ></input>
+        <label htmlFor="postTitle">Post:</label>
+        <textarea
+          id="postBody"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        ></textarea>
+        <button type="submit">submit</button>
+      </form>
     </main>
   );
 }
