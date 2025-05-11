9f5c688672b54a2517657ffc2b749a57c6696868
Mario-Gozie <oguedoihucg@gmail.com>
2025-05-11 23:52:28 +0300

    working on a lot

--- 04tutorial/src/Home.js
+++ 04tutorial/src/Home.js
@@ -1,33 +1,33 @@
-// import React from "react";
-// import Feed from "./Feed";
-// // import { useContext } from "react";
-// // import DataContext from "./Context/DataContext";
-// import { useStoreState } from "easy-peasy";
+import React from "react";
+import Feed from "./Feed";
+// import { useContext } from "react";
+// import DataContext from "./Context/DataContext";
+import { useStoreState } from "easy-peasy";
 
-// function Home({ isLoading, fetchError }) {
-//   // const { searchResult, fetchError, isLoading } = useContext(DataContext);
+function Home({ isLoading, fetchError }) {
+  // const { searchResult, fetchError, isLoading } = useContext(DataContext);
 
-//   const { searchResults } = useStoreState((state) => state.searchResults);
+  const { searchResults } = useStoreState((state) => state.searchResults);
 
-//   return (
-//     <main className="Home">
-//       {isLoading && <p className="statusMsg">Loading posts...</p>}
-//       {!isLoading && fetchError && (
-//         <p className="statusMsg" style={{ color: "red" }}>
-//           {fetchError}
-//         </p>
-//       )}
-//       {!isLoading &&
-//         !fetchError &&
-//         (searchResults.length ? (
-//           <Feed posts={searchResults}></Feed>
-//         ) : (
-//           <p className="statusMsg" style={{ marginTop: "2rem" }}>
-//             No Post to display
-//           </p>
-//         ))}
-//     </main>
-//   );
-// }
+  return (
+    <main className="Home">
+      {isLoading && <p className="statusMsg">Loading posts...</p>}
+      {!isLoading && fetchError && (
+        <p className="statusMsg" style={{ color: "red" }}>
+          {fetchError}
+        </p>
+      )}
+      {!isLoading &&
+        !fetchError &&
+        (searchResults.length ? (
+          <Feed posts={searchResults}></Feed>
+        ) : (
+          <p className="statusMsg" style={{ marginTop: "2rem" }}>
+            No Post to display
+          </p>
+        ))}
+    </main>
+  );
+}
 
-// export default Home;
+export default Home;
